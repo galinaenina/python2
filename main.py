91288a3dfb89709c00cc79f4db9b2ce86ebb8e9c
Galina_Enina <kegn5@mail.ru>
2022-12-13 19:28:16 +0300

    задание 2

--- main.py
+++ main.py
@@ -1,51 +1,41 @@
-a = 1
-b = 2
-c = 3
-d = 4
-f = a + b + c - d
-print (f)
+number = 44444
+text = 'задание'
+mix = 3, 4, 7, 'решение'
+print(type(number))
+print(type(text))
+print(type(mix))
 
-time_sec = int(input('Введите число: '))
-hours = time_sec//3600
-hours_res = (hours) if hours > 10 else ('0' + str(hours))
-minutes = (time_sec % 3600)//60
-minutes_res = (minutes) if minutes > 10 else ('0' + str(minutes))
-seconds = (time_sec % 3600) % 60
-seconds_res = (seconds) if seconds > 10 else ('0' + str(seconds))
-if hours > 24:
-    print('Количество полученных часов превышает количество часов в сутка, скоректируйте ввод.')
-else:
-    print(f'Московское время: {hours_res}:{minutes_res}:{seconds_res}')
+s = [int(i) for i in input("Введите числа через пробел: ").split(" ")]
+for i in range(1, len(s)-1, 2):
+    s[i-1], s[i] = s[i], s[i-1]
+print(''.join([str(i) for i in s]))
 
-n = input('Введите число: ')
-nn = int(n+n)
-nnn = int(n+n+n)
-n = int(n)
-result = n+nn+nnn
-print(result)
+m = int(input("Введите номер месяца: "))
+x = {'Зима': (12, 1, 2), 'Весна': (3, 4, 5), 'Лето': (6, 7, 8),'Осень': (9, 10, 11)}
+[print(a) for a, b in x.items() if m in b]
 
-a = input('Введите число больше нуля:')
-maxmin = max(int(x) for x in a)
-print(maxmin)
+list = input('Введите слова через пробел: ').split()
+for i, el in enumerate(list):
+    print(i, el[0:10])
 
-plus = int(input('Введите значение прибыли: '))
-minus = int(input('Введите значение издержек: '))
-people = int(input('Ввдите количество работников: '))
-if plus > minus:
-    print('Выручка больше издержек')
-    clear_plus = plus - minus
-    rent = clear_plus/plus
-    print('Рентабельность {} выручки {}: {:.2f}' .format('нашей','составила',rent))
-    clear_for_person = float(clear_plus/people)
-    print('Прибыль фирмы в расчете на одного сотрудника: %s'%clear_for_person)
-else:
-    print('Фирма работает в убыток')
+my_list = [5, 4, 3, 2, 1]
+print(f"Рейтинг - {my_list}")
+digit = int(input("Введите число (000 - выход): "))
+while digit != 000:
+    for el in range(len(my_list)):
+        if my_list[el] == digit:
+            my_list.insert(el + 1, digit)
+            break
+        elif my_list[0] < digit:
+            my_list.insert(0, digit)
+        elif my_list[-1] > digit:
+            my_list.append(digit)
+        elif my_list[el] > digit and my_list[el + 1] < digit:
+            my_list.insert(el + 1, digit)
+    print(f"текущий список - {my_list}")
+    digit = int(input("Введите число (000 - выход) "))
 
-a = int(input('Введите расстояние пробежки: '))
-в = int(input('Введите максимальное расстояние пробежки: '))
-day = 1
-while a < b:
-    a = a + (a * 0.1)
-day += 1
-print(f"На {day}-й день спортсмен достиг результата - не менее {b} км")
+
+
+
 
