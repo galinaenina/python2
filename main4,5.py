5770b36e6c16faf91980c241613a715ae628320e
Galina_Enina <kegn5@mail.ru>
2022-12-25 18:12:11 +0300

    задание 4,5

---
+++ main4,5.py
@@ -0,0 +1,67 @@
+import random
+
+file=open("C:\\Users\\kegn5\\Desktop\\file2.txt", "w")
+text = input()
+while text != "":
+    file.write(text)
+    file.write("\n")
+    text = input()
+file.close()
+
+
+fileRead=open("C:\\Users\\kegn5\\Desktop\\file2.txt", "r")
+k = fileRead.readline()
+i = 0
+while k != "":
+  print("Число слов в ", i, " строке равно:")
+  if k == "\n":
+      print(0)
+  else:
+      print(len(k.split(" ")))
+  i=i+1
+  k = fileRead.readline()
+print ("Число строк", i)
+
+fileRead2=open("C:\\Users\\kegn5\\Desktop\\file3.txt", "r", encoding="utf8")
+k = fileRead2.readline()
+summa = 0
+count = 0
+while k != "":
+    a = k.split(" ")
+    summa = summa + int(a[1])
+    count = count + 1
+    if int(a[1])<20000:
+        print(a[0])
+    k = fileRead2.readline()
+print(summa/count)
+
+fileRead4=open("C:\\Users\\kegn5\\Desktop\\file4.txt", "r")
+file7=open("C:\\Users\\kegn5\\Desktop\\file7.txt", "w")
+k = fileRead4.readline()
+while k != "":
+    print(k)
+    if k=='One - 1\n':
+        print('один - 1')
+        file7.write('один - 1')
+    if k=='Two - 2\n':
+        print('два - 2')
+        file7.write('два - 2')
+    if k=='Three - 3\n':
+        print('три - 3')
+        file7.write('три - 3')
+    if k=='Four - 4\n':
+        print('четыре - 4')
+        file7.write('четыре - 4')
+    k = fileRead4.readline()
+
+file=open("C:\\Users\\kegn5\\Desktop\\file5.txt", "w")
+summa = 0
+for i in range(15):
+    a = random.randint(10, 20)
+    summa = summa + a
+    file.write(str(a))
+    file.write(" ")
+print(summa)
+file.close()
+
+
